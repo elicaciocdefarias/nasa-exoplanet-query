3a382859b5b24fa939e4daeff31bc005d2c3c0df
Elicácio C. de Farias <elicaciocardoso@gmail.com>
2021-06-22 18:32:51 -0300

    add file handling to the upload view

--- search/views.py
+++ search/views.py
@@ -1,3 +1,4 @@
+from search.handlers import HandleFile
 from search.forms import ExoplanetUploadCSVFileForm
 from django.views.generic import TemplateView, FormView, ListView
 from search.models import Exoplanet
@@ -16,3 +17,12 @@ class ExoplanetUploadView(FormView):
     template_name = "search/upload_csv.html"
     form_class = ExoplanetUploadCSVFileForm
     success_url = "/list/"
+
+    def form_valid(self, form):
+        file = form.cleaned_data["file"]
+        handle_file = HandleFile(file)
+        if handle_file.is_type_csv() and handle_file.structure_is_valid():
+            for record in handle_file.records():
+                print(record)
+            return super().form_valid(form)
+        return super().form_invalid(form)
